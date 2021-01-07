565d8c81b26d2d89931bb18b73c3af9097555c16
David Eads <deads@redhat.com>
2021-01-07 14:24:41 -0500

    add kube data collection script

---
+++ scripts/fetchdata-kube.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# sleep before fetching so that if we're in some sort of fast crashloop/reschedule mode, 
+# we don't ping testgrid everytime we come back up
+echo "Doing initial sleep before fetching testgrid data"
+sleep 60 # 1 minutes
+while [ true ]; do
+  echo "Fetching new testgrid data"
+  rm -rf /data/*
+  /tmp/src/sippy --fetch-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= -v 4
+  echo "Done fetching data, refreshing server"
+  curl localhost:8080/refresh
+  echo "Done refreshing data, sleeping"
+  sleep 3600  # 1 hour
+done
