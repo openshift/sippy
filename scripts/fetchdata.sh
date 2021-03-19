76196866858ba2fd71109511c5700b2ee8f46300
Ben Parees <bparees@redhat.com>
2021-03-19 10:38:03 -0400

    remove obsolete ocp versions from sippy reporting

--- scripts/fetchdata.sh
+++ scripts/fetchdata.sh
@@ -7,7 +7,7 @@ sleep 600 # 10 minutes
 while [ true ]; do
   echo "Fetching new testgrid data"
   rm -rf /data/*
-  /go/src/sippy/sippy -v 4 --fetch-data /data --release 3.11 --release 4.2 --release 4.3 --release 4.4 --release 4.5 --release 4.6 --release 4.7 --release 4.8
+  /go/src/sippy/sippy -v 4 --fetch-data /data --release 3.11 --release 4.4 --release 4.5 --release 4.6 --release 4.7 --release 4.8
   echo "Done fetching data, refreshing server"
   curl localhost:8080/refresh
   echo "Done refreshing data, sleeping"
