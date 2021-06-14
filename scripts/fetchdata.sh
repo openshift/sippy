f0552299ceff78961eddfe1e81158db9ea2bb910
Stephen Benjamin <stephen@redhat.com>
2021-06-14 13:31:54 -0400

    Add v4.9 release to sippy deployment/data fetch

--- scripts/fetchdata.sh
+++ scripts/fetchdata.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# sleep before fetching so that if we're in some sort of fast crashloop/reschedule mode, 
+# sleep before fetching so that if we're in some sort of fast crashloop/reschedule mode,
 # we don't ping testgrid everytime we come back up
 echo "Doing initial sleep before fetching testgrid data"
 sleep 600 # 10 minutes
 while [ true ]; do
   echo "Fetching new testgrid data"
   rm -rf /data/*
-  /go/src/sippy/sippy -v 4 --fetch-data /data --release 3.11 --release 4.4 --release 4.5 --release 4.6 --release 4.7 --release 4.8
+  /go/src/sippy/sippy -v 4 --fetch-data /data --release 3.11 --release 4.4 --release 4.5 --release 4.6 --release 4.7 --release 4.8 --release 4.9
   echo "Done fetching data, refreshing server"
   curl localhost:8080/refresh
   echo "Done refreshing data, sleeping"
