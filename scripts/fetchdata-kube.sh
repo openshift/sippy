9c137d9553b896fce0cc19ff73d8aca2689243e5
Devan Goodwin <dgoodwin@redhat.com>
2022-05-11 08:31:31 -0300

    Specify variant=kube when loading database.

--- scripts/fetchdata-kube.sh
+++ scripts/fetchdata-kube.sh
@@ -9,7 +9,7 @@ while [ true ]; do
   rm -rf /data/*
   /bin/sippy --fetch-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= --log-level debug
   echo "Loading database"
-  /bin/sippy --load-database --local-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= --log-level debug
+  /bin/sippy --load-database --local-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= --log-level debug --variant=kube
   echo "Done fetching data, refreshing server"
   curl localhost:8080/refresh
   echo "Done refreshing data, sleeping"
