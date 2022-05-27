de290394c8bb5e79e599e7cc4ce549e08944f65d
Devan Goodwin <dgoodwin@redhat.com>
2022-05-27 13:22:45 -0300

    Vendor and use gorilla/mux for routing.

--- pkg/sippyserver/server.go
+++ pkg/sippyserver/server.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -852,7 +853,7 @@ func (s *Server) jsonPerfScaleMetricsReport(w http.ResponseWriter, req *http.Req
 
 func (s *Server) Serve() {
 	// Use private ServeMux to prevent tests from stomping on http.DefaultServeMux
-	serveMux := http.NewServeMux()
+	serveMux := mux.NewRouter()
 
 	// Handle serving React version of frontend with support for browser router, i.e. anything not found
 	// goes to index.html
