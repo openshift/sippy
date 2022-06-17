987b6d6d46fb82e31c33112051b15003a1ad1857
Devan Goodwin <dgoodwin@redhat.com>
2022-06-17 13:55:20 -0300

    Refactor metrics code into a separate package.

---
+++ pkg/sippyserver/metrics/metrics.go
@@ -0,0 +1,75 @@
+package metrics
+
+import (
+	"time"
+
+	"github.com/openshift/sippy/pkg/api"
+	sippyprocessingv1 "github.com/openshift/sippy/pkg/apis/sippyprocessing/v1"
+	"github.com/openshift/sippy/pkg/db"
+	"github.com/openshift/sippy/pkg/db/query"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	jobPassRatioMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "sippy_job_pass_ratio",
+		Help: "Ratio of passed job runs for the given job in a period (2 day, 7 day, etc)",
+	}, []string{"release", "period", "name"})
+	releaseWarningsMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "sippy_release_warnings",
+		Help: "Number of current warnings for a release, see overview page in UI for details",
+	}, []string{"release"})
+)
+
+func RefreshMetricsDB(dbc *db.DB) error {
+	releases, err := query.ReleasesFromDB(dbc)
+	if err != nil {
+		return err
+	}
+
+	promReportTypes := buildPromReportTypes(releases)
+	if err != nil {
+		return err
+	}
+
+	for _, pType := range promReportTypes {
+		// start, boundary and end will just be defaults
+		// the api will decide based on the period
+		// and current day / time
+		jobsResult, err := api.JobReportsFromDB(dbc, pType.release, pType.period, nil, time.Time{}, time.Time{}, time.Time{})
+
+		if err != nil {
+			return errors.Wrapf(err, "error refreshing prom report type %s - %s", pType.period, pType.release)
+		}
+		for _, jobResult := range jobsResult {
+			jobPassRatioMetric.WithLabelValues(pType.release, pType.period, jobResult.Name).Set(jobResult.CurrentPassPercentage / 100)
+		}
+	}
+
+	// Add a metric for any warnings for each release. We can't convey exact details with prom, but we can
+	// tell you x warnings are present and link you to the overview in the alert.
+	for _, release := range releases {
+		releaseWarnings := api.ScanForReleaseWarnings(dbc, release.Release)
+		releaseWarningsMetric.WithLabelValues(release.Release).Set(float64(len(releaseWarnings)))
+	}
+
+	return nil
+}
+
+type promReportType struct {
+	release string
+	period  string
+}
+
+func buildPromReportTypes(releases []query.Release) []promReportType {
+	var promReportTypes []promReportType
+
+	for _, release := range releases {
+		promReportTypes = append(promReportTypes, promReportType{release: release.Release, period: string(sippyprocessingv1.TwoDayReport)})
+		promReportTypes = append(promReportTypes, promReportType{release: release.Release, period: string(sippyprocessingv1.CurrentReport)})
+	}
+
+	return promReportTypes
+}
