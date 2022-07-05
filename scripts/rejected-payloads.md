2980cfa603c0457ffa4b7852f436e48cd1c415a1
xueqzhan <kenzhang@redhat.com>
2022-07-05 16:02:26 -0400

    Add script to categorize payload

---
+++ scripts/rejected-payloads.md
@@ -0,0 +1,85 @@
+## What is this
+
+rejected-payload is a python script used to categorize the reject-reason column of the release_tags table of the Sippy DB.
+reject-reason column is used to indicate, for a particular rejected payload, why it was rejected.
+This is intended to be used by the watcher as he monitors the status of each payload.
+The goal is to provide us some statistics of payload rejection history.
+
+Here is the list of known categories:
+
+- "TEST_FLAKE",
+- "CLOUD_INFRA",
+- "RH_INFRA",
+- "PRODUCT_REGRESSION",
+- "TEST_REGRESSION"
+
+## Python Dependencies
+
+Use pip to install the following dependencies:
+
+`pip install argparse sqlalchemy`
+
+## Key Functions
+
+### List
+List command can be used to list uncategorized release tags.
+Additional options can be provided to limit the scope of the query.
+
+```
+  -r RELEASE, --release RELEASE    Specifies a release, like 4.11
+  -s STREAM, --stream STREAM       Specifies a stream, like nightly or ci
+  -a, --all                        List all rejected payloads. If not specified , list only uncategorized ones.
+```
+
+The following example lists all uncategorized release tags for 4.11 ci payloads:
+
+```
+% python ./rejected-payloads.py -d "your-dsn" list -s ci -r 4.11
+index     release tag                                       phase               reject reason
+1         4.11.0-0.ci-2022-06-29-121424                     Rejected            None
+2         4.11.0-0.ci-2022-06-28-211909                     Rejected            None
+```
+
+### Categorize
+Categorize is used to update the reject-reason in the DB.
+It can be used to update a single release tag when -t or --release_tag is specified.
+If no release_tag is specified, it will find all uncategorized release tags and provide an interactive prompt to update each release tag.
+
+```
+  -t RELEASE_TAG, --release_tag RELEASE_TAG     Specifies a release payload tag, like 4.11.0-0.nightly-2022-06-25-081133
+  -r RELEASE, --release RELEASE                 Specifies a release, like 4.11
+  -s STREAM, --stream STREAM                    Specifies a stream, like nightly or ci
+```
+
+Here is a single update example:
+
+```
+% python ./rejected-payloads.py -d "your-dsn" categorize -t 4.11.0-0.nightly-2022-06-28-111405
+Please choose the reject reason for tag 4.11.0-0.nightly-2022-06-28-111405 from the following list:
+         1:           TEST_FLAKE
+         2:          CLOUD_INFRA
+         3:             RH_INFRA
+         4:   PRODUCT_REGRESSION
+         5:      TEST_REGRESSION
+Enter your selection between 1 and 5: 2
+```
+
+Here is a group update example:
+
+```
+% python ./rejected-payloads.py -d "your-dsn" categorize -s ci
+index     release tag                                       phase               reject reason
+1         4.11.0-0.ci-2022-06-29-121424                     Rejected            None
+2         4.11.0-0.ci-2022-06-28-211909                     Rejected            None
+Select tag between 1 and 2 to categorize, enter q to exit: 1
+Please choose the reject reason for tag 4.11.0-0.ci-2022-06-29-121424 from the following list:
+         1:           TEST_FLAKE
+         2:          CLOUD_INFRA
+         3:             RH_INFRA
+         4:   PRODUCT_REGRESSION
+         5:      TEST_REGRESSION
+Enter your selection between 1 and 5: 3
+index     release tag                                       phase               reject reason
+1         4.11.0-0.ci-2022-06-28-211909                     Rejected            None
+Select tag between 1 and 1 to categorize, enter q to exit: q
+```
---
+++ scripts/rejected-payloads.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import argparse
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+base = declarative_base()
+
+class ReleaseTags(base):
+    __tablename__ = 'release_tags'
+
+    id = Column(String, primary_key=True)
+    release_tag = Column(String)
+    release = Column(String)
+    stream = Column(String)
+    phase = Column(String)
+    reject_reason = Column(String)
+
+def list(session, release, stream, showAll):
+    selectedTags = []
+    releaseTags = session.query(ReleaseTags).filter(ReleaseTags.phase == "Rejected").all()
+    for releaseTag in releaseTags:
+        if release and releaseTag.release != release:
+            continue
+        if stream and releaseTag.stream != stream:
+            continue
+        if not showAll and releaseTag.reject_reason:
+            continue
+        selectedTags.append(releaseTag)
+
+    print("%-10s%-50s%-20s%-20s" % ("index", "release tag", "phase", "reject reason"))
+    for idx, releaseTag in enumerate(selectedTags):
+        print("%-10d%-50s%-20s%-20s" % (idx+1, releaseTag.release_tag, releaseTag.phase, releaseTag.reject_reason))
+
+    return selectedTags
+
+def categorizeSingle(session, releaseTag):
+    reject_reasons = ["TEST_FLAKE", "CLOUD_INFRA", "RH_INFRA", "PRODUCT_REGRESSION", "TEST_REGRESSION"]
+    releaseTags = session.query(ReleaseTags).filter(ReleaseTags.release_tag == releaseTag).all()
+    for releaseTag in releaseTags:
+        print("Please choose the reject reason for tag %s from the following list:" % releaseTag.release_tag)
+        for idx, reason in enumerate(reject_reasons):
+            print("%10d: %20s" % (idx+1, reason))
+
+        while True:
+            val = input("Enter your selection between 1 and " + str(len(reject_reasons)) + ": ")
+            try:
+                index = int(val)
+                if index > 0 and index <= len(reject_reasons):
+                    break
+            except ValueError:
+                continue
+        releaseTag.reject_reason = reject_reasons[index-1]
+    session.commit()
+
+def categorize(session, release, stream):
+    while True:
+        selectedTags = list(session, release, stream, False)
+        if len(selectedTags) == 0:
+            break
+        val = input("Select tag between 1 and " + str(len(selectedTags)) + " to categorize, enter q to exit: ")
+        if val == "q":
+            break
+        try:
+            index = int(val)
+            if index > 0 and index <= len(selectedTags):
+                categorizeSingle(session, selectedTags[index-1].release_tag)
+        except ValueError:
+            continue
+    session.commit()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='View or Update payload reject reasons to DB')
+    parser.add_argument('-d', '--dsn', help='Specifies the DSN used to connect to DB', required=True)
+    subparsers = parser.add_subparsers(title='subcommands', description='valid subcommands', help='Supported operations', required=True)
+    list_parser = subparsers.add_parser('list', help='list rejected payloads')
+    list_parser.set_defaults(action='list')
+    list_parser.add_argument('-r', '--release', help='Specifies a release, like 4.11', default=None)
+    list_parser.add_argument('-s', '--stream', help='Specifies a stream, like nightly or ci', default=None)
+    list_parser.add_argument('-a', '--all', help='List all rejected payloads. If not specified , list only uncategorized ones.', action='store_true')
+
+    categorize_parser = subparsers.add_parser('categorize', help='categorize a rejected payload')
+    categorize_parser.set_defaults(action='categorize')
+    categorize_parser.add_argument('-t', '--release_tag', help='Specifies a release payload tag, like 4.11.0-0.nightly-2022-06-25-081133', default=None)
+    categorize_parser.add_argument('-r', '--release', help='Specifies a release, like 4.11', default=None)
+    categorize_parser.add_argument('-s', '--stream', help='Specifies a stream, like nightly or ci', default=None)
+
+    args = vars(parser.parse_args())
+
+    #dsn = "postgresql://postgres:@localhost:5432/postgres"
+    db = create_engine(args["dsn"])
+
+    Session = sessionmaker(db)
+    session = Session()
+
+    base.metadata.create_all(db)
+
+    if args["action"] == "categorize":
+        if args["release_tag"]:
+            categorizeSingle(session, args["release_tag"])
+        else:
+            categorize(session, args["release"], args["stream"])
+    else:
+        list(session, args["release"], args["stream"], args["all"])
+
