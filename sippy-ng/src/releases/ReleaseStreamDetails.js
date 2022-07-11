0d6cebf5440251ad429feaf5ed4605f1911cd133
Devan Goodwin <dgoodwin@redhat.com>
2022-07-11 09:54:59 -0300

    Stop making expensive analysis api call in details page.

--- sippy-ng/src/releases/ReleaseStreamDetails.js
+++ sippy-ng/src/releases/ReleaseStreamDetails.js
@@ -1,11 +1,5 @@
 import { Alert } from '@material-ui/lab'
-import {
-  Card,
-  Container,
-  Grid,
-  makeStyles,
-  Typography,
-} from '@material-ui/core'
+import { Container, Grid, makeStyles, Typography } from '@material-ui/core'
 import { filterFor, safeEncodeURIComponent } from '../helpers'
 import { Fragment } from 'react'
 import { Link, Route, Switch, useRouteMatch } from 'react-router-dom'
@@ -27,12 +21,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ReleaseStreamDetails(props) {
   const classes = useStyles()
-  const { path, url } = useRouteMatch()
-
-  const [fetchError, setFetchError] = React.useState('')
-  const [isLoaded, setLoaded] = React.useState(false)
-
-  const [analysis, setAnalysis] = React.useState({})
 
   const [release = props.release, setRelease] = useQueryParam(
     'release',
@@ -43,67 +31,6 @@ export default function ReleaseStreamDetails(props) {
     'stream',
     StringParam
   )
-
-  const fetchData = () => {
-    /*
-    const filter = safeEncodeURIComponent(
-      JSON.stringify({
-        items: [filterFor('release_tag', 'equals', releaseTag)],
-      })
-    )
-     */
-
-    Promise.all([
-      fetch(
-        `${process.env.REACT_APP_API_URL}/api/releases/stream_analysis?release=${release}&arch=${arch}&stream=${stream}`
-      ),
-    ])
-      .then(([analysis]) => {
-        if (analysis.status !== 200) {
-          throw new Error('server returned ' + analysis.status)
-        }
-
-        return Promise.all([analysis.json()])
-      })
-      .then(([analysis]) => {
-        if (analysis.length === 0) {
-          return (
-            <Typography variant="h5">
-              Analysis #{analysis} not found.
-            </Typography>
-          )
-        }
-        console.log('GOT ANALYSIS')
-        console.log(analysis)
-
-        setAnalysis(analysis)
-        setLoaded(true)
-      })
-      .catch((error) => {
-        setFetchError(
-          'Could not retrieve payload stream analysis for ' +
-            props.release +
-            ' ' +
-            props.arch +
-            ' ' +
-            props.stream +
-            ': ' +
-            error
-        )
-      })
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  if (fetchError !== '') {
-    return <Alert severity="error">Failed to load data, {fetchError}</Alert>
-  }
-
-  if (!isLoaded) {
-    return <p>Loading...</p>
-  }
 
   let currPage = props.arch + ' ' + props.stream
 
