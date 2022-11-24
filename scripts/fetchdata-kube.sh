b0def55448f66a9ccc0492eed434ed3819aaca0e
Devan Goodwin <dgoodwin@redhat.com>
2022-11-24 14:07:49 -0400

    Prep kube fetchdata script

--- scripts/fetchdata-kube.sh
+++ scripts/fetchdata-kube.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-# sleep before fetching so that if we're in some sort of fast crashloop/reschedule mode,
-# we don't ping testgrid everytime we come back up
-echo "Doing initial sleep before fetching testgrid data"
-sleep 60 # 1 minutes
-while [ true ]; do
-  echo "Fetching new testgrid data"
-  rm -rf /data/*
-  /bin/sippy --fetch-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= --log-level debug
-  echo "Loading database"
-  /bin/sippy --load-database --local-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= --log-level debug --mode=kube
-  echo "Done fetching data, refreshing server"
-  curl localhost:8080/refresh
-  echo "Done refreshing data, sleeping"
-  sleep 3600  # 1 hour
-done
+echo "Fetching new testgrid data"
+rm -rf /data/*
+/bin/sippy --fetch-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= --log-level debug
+echo "Loading database"
+/bin/sippy --load-database --local-data /data --dashboard=kube-master=sig-release-master-blocking,sig-release-master-informing= --log-level debug --mode=kube
+echo "Done fetching data"
