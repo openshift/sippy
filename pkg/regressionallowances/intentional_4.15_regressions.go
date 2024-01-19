3c132366cef249c467bedca7271ead89782a82ad
David Eads <deads@redhat.com>
2024-01-19 12:07:45 -0500

    allow exception for SDN installation failure

--- pkg/regressionallowances/intentional_4.15_regressions.go
+++ pkg/regressionallowances/intentional_4.15_regressions.go
@@ -1,5 +1,7 @@
 package regressionallowances
 
+import "github.com/openshift/sippy/pkg/apis/api"
+
 //nolint:all
 func init() {
 	/*
@@ -22,4 +24,23 @@ func init() {
 				ReasonToAllowInsteadOfFix: "",
 			})
 	*/
+
+	mustAddIntentionalRegression(
+		release415,
+		IntentionalRegression{
+			JiraComponent: "Networking / openshift-sdn",                       // Jira Component,  not team name
+			TestID:        "cluster install:0cb1bb27e418491b1ffdacab58c5c8c0", // ask TRT for the ID for your TestName
+			TestName:      "install should succeed: overall",                  // this helps approvers recognize at a glance
+			Variant: api.ComponentReportColumnIdentification{ // this indicates the selectivity of the choice
+				Network:  "sdn",
+				Upgrade:  "upgrade-micro",
+				Arch:     "amd64",
+				Platform: "metal-ipi",
+			},
+			PreviousPassPercentage:    100,
+			PreviousSampleSize:        61, // number of runs used for the percentage
+			RegressedPassPercentage:   50,
+			RegressedSampleSize:       12, // number of runs used for the percentage
+			ReasonToAllowInsteadOfFix: "I can't explain this, but Ben Bennett maybe?",
+		})
 }
