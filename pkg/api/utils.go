31d0b3f83d5ffa4388e90b0f692558fc0ea587b8
Forrest Babcock <fbabcock@redhat.com>
2024-02-12 13:50:17 -0500

    trt-1555: support bigquery table for triaged incidents

--- pkg/api/utils.go
+++ pkg/api/utils.go
@@ -6,6 +6,8 @@ import (
 	"reflect"
 	"time"
 
+	"github.com/pkg/errors"
+
 	bqclient "github.com/openshift/sippy/pkg/bigquery"
 	"github.com/openshift/sippy/pkg/db"
 	"github.com/openshift/sippy/pkg/db/query"
@@ -20,36 +22,60 @@ var (
 
 const releasePresubmits = "Presubmits"
 
-// getReportFromCacheOrGenerate attempts to find a cached record otherwise generates a new report.
-func getReportFromCacheOrGenerate[T any](c cache.Cache, cacheOptions cache.RequestOptions, cacheKey interface{}, generateFn func() (T, []error), defaultVal T) (T, []error) {
-	// If someone is giving us an uncacheable cacheKey, we should panic so it gets detected in testing
+type CacheData struct {
+	cacheKey func() ([]byte, error)
+}
+
+func (c *CacheData) GetCacheKey() ([]byte, error) {
+	if c.cacheKey == nil {
+		panic("cache key is nil")
+	}
+	return c.cacheKey()
+}
+
+func GetPrefixedCacheKey(prefix string, cacheKey interface{}) CacheData {
 	if isStructWithNoPublicFields(cacheKey) {
 		panic(fmt.Sprintf("you cannot use struct %s with no exported fields as a cache key", reflect.TypeOf(cacheKey)))
-	} else if cacheKey == "" {
-		panic(fmt.Sprintf("you cannot use empty string as a cache key for %s", reflect.TypeOf(defaultVal)))
-	} else if cacheKey == nil {
-		panic(fmt.Sprintf("cache key is nil for %s", reflect.TypeOf(defaultVal)))
 	}
 
+	return CacheData{cacheKey: func() ([]byte, error) {
+		b, err := json.Marshal(cacheKey)
+		if err != nil {
+			return nil, err
+		}
+		if len(prefix) > 0 {
+			return append([]byte(prefix), b...), nil
+		}
+		return b, nil
+	}}
+}
+
+// getDataFromCacheOrGenerate attempts to find a cached record otherwise generates new data.
+func getDataFromCacheOrGenerate[T any](c cache.Cache, cacheOptions cache.RequestOptions, cacheData CacheData, generateFn func() (T, []error), defaultVal T) (T, []error) {
 	if c != nil {
-		jsonCacheKey, err := json.Marshal(cacheKey)
+		cacheKey, err := cacheData.GetCacheKey()
 		if err != nil {
 			return defaultVal, []error{err}
 		}
 
+		// If someone is giving us an uncacheable cacheKey, we should panic so it gets detected in testing
+		if len(cacheKey) == 0 {
+			panic(fmt.Sprintf("cache key is empty for %s", reflect.TypeOf(defaultVal)))
+		}
+
 		if !cacheOptions.ForceRefresh {
-			if res, err := c.Get(string(jsonCacheKey)); err == nil {
+			if res, err := c.Get(string(cacheKey)); err == nil {
 				log.WithFields(log.Fields{
-					"key":  string(jsonCacheKey),
+					"key":  string(cacheKey),
 					"type": reflect.TypeOf(defaultVal).String(),
 				}).Debugf("cache hit")
 				var cr T
 				if err := json.Unmarshal(res, &cr); err != nil {
-					return defaultVal, []error{err}
+					return defaultVal, []error{errors.WithMessagef(err, "failed to unmarshal cached item.  cacheKey=%+v", cacheKey)}
 				}
 				return cr, nil
 			}
-			log.Infof("cache miss for cache key: %s", string(jsonCacheKey))
+			log.Infof("cache miss for cache key: %s", string(cacheKey))
 		}
 		result, errs := generateFn()
 		if len(errs) == 0 {
@@ -61,10 +87,10 @@ func getReportFromCacheOrGenerate[T any](c cache.Cache, cacheOptions cache.Reque
 					// Only cache until the next rounding duration
 					cacheDuration = now.Truncate(cacheOptions.CRTimeRoundingFactor).Add(cacheOptions.CRTimeRoundingFactor).Sub(now)
 				}
-				if err := c.Set(string(jsonCacheKey), cr, cacheDuration); err != nil {
+				if err := c.Set(string(cacheKey), cr, cacheDuration); err != nil {
 					log.WithError(err).Warningf("couldn't persist new item to cache")
 				} else {
-					log.Debugf("cache set for cache key: %s", string(jsonCacheKey))
+					log.Debugf("cache set for cache key: %s", string(cacheKey))
 				}
 			}
 		}
