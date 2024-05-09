749a9c53bf6480b5e298177fc416e03e19f04d77
Dennis Periquet <dperique@redhat.com>
2024-05-09 12:34:48 -0400

    identify link as an API URL

--- sippy-ng/src/component_readiness/CopyPageURL.js
+++ sippy-ng/src/component_readiness/CopyPageURL.js
@@ -1,19 +1,18 @@
 import { Box, Button, Tooltip, Typography } from '@mui/material'
+import Link from '@mui/material/Link'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 // This is used as a convenient way to capture the page URL for use with
 // the triaging workflow.
 export default function CopyPageURL(props) {
-  const handleCopyClick = () => {
-    navigator.clipboard.writeText(props.apiCallStr)
-  }
-
   return (
     <Box align="right">
       <Typography variant="caption">
         <Tooltip title={props.apiCallStr}>
-          <Button onClick={handleCopyClick}>copy page URL</Button>
+          <Link href={props.apiCallStr} target="_blank">
+            API URL
+          </Link>
         </Tooltip>
       </Typography>
     </Box>
