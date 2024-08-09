0005d0d4684fc0f5befcf07d55050f84c08475bb
Luke Meyer <lmeyer@redhat.com>
2024-08-09 08:53:16 -0400

    enable variants to compare

--- sippy-ng/src/component_readiness/CheckboxList.js
+++ sippy-ng/src/component_readiness/CheckboxList.js
@@ -3,10 +3,13 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  Button,
   Checkbox,
   FormControl,
   FormControlLabel,
   FormGroup,
+  Grid,
+  Tooltip,
 } from '@mui/material'
 import { ExpandMore } from '@mui/icons-material'
 import { makeStyles } from '@mui/styles'
@@ -33,11 +36,19 @@ export default function CheckBoxList(props) {
       margin: '0px !important',
       padding: '0px',
     },
+    gridCenter: {
+      marginTop: theme.spacing(1),
+      textAlign: 'center',
+    },
+    gridRight: {
+      justifyContent: 'flex-end',
+      display: 'flex',
+    },
   }))
 
   const classes = useStyles()
-  const checkedItems = props.checkedItems
-  const setCheckedItems = props.setCheckedItems
+  const [checkedItems, setCheckedItems] = useState(props.checkedItems)
+  const [isCompareMode, setIsCompareMode] = useState(false)
   const handleChange = (event) => {
     const item = event.target.name
     const isChecked = event.target.checked
@@ -48,6 +59,10 @@ export default function CheckBoxList(props) {
         checkedItems.filter((checkedItem) => checkedItem !== item)
       )
     }
+  }
+  const handleToggleCompare = () => {
+    console.log('Toggle Compare')
+    setIsCompareMode(!isCompareMode)
   }
 
   return (
@@ -66,28 +81,93 @@ export default function CheckBoxList(props) {
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <FormGroup>
-            {props.displayList.map((item) => (
-              <FormControlLabel
-                key={item}
-                control={
-                  <Checkbox
-                    checked={checkedItems.includes(item)}
-                    onChange={handleChange}
-                    name={item}
-                  />
-                }
-                label={item}
-              />
-            ))}
-          </FormGroup>
+          {VariantGroup(
+            props,
+            classes,
+            isCompareMode,
+            checkedItems,
+            handleChange
+          )}
+          <Tooltip
+            title={'Compare with different variants for sample and basis'}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.control}
+              onClick={handleToggleCompare}
+            >
+              {isCompareMode ? 'Cancel Compare' : 'Compare'}
+            </Button>
+          </Tooltip>
         </AccordionDetails>
       </Accordion>
     </FormControl>
   )
 }
 
-CheckBoxList.propTypes = {
+function VariantGroup(
+  props,
+  classes,
+  isCompareMode,
+  checkedItems,
+  handleChange
+) {
+  if (isCompareMode) {
+    return (
+      <FormGroup>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            Basis
+          </Grid>
+          <Grid item xs={6} className={classes.gridRight}>
+            Sample
+          </Grid>
+        </Grid>
+        {props.displayList.map((item) => (
+          <Grid container spacing={2} key={item}>
+            <Grid item xs={2}>
+              <Checkbox
+                checked={checkedItems.includes(item)}
+                onChange={handleChange}
+                name={item}
+              />
+            </Grid>
+            <Grid item xs={8} className={classes.gridCenter}>
+              {item}
+            </Grid>
+            <Grid item xs={2}>
+              <Checkbox
+                checked={checkedItems.includes('sample_' + item)}
+                onChange={handleChange}
+                name={'sample_' + item}
+              />
+            </Grid>
+          </Grid>
+        ))}
+      </FormGroup>
+    )
+  }
+  return (
+    <FormGroup>
+      {props.displayList.map((item) => (
+        <FormControlLabel
+          key={item}
+          control={
+            <Checkbox
+              checked={checkedItems.includes(item)}
+              onChange={handleChange}
+              name={item}
+            />
+          }
+          label={item}
+        />
+      ))}
+    </FormGroup>
+  )
+}
+
+CheckBoxList.propTypes = VariantGroup.propTypes = {
   headerName: PropTypes.string,
   displayList: PropTypes.array,
   checkedItems: PropTypes.array,
