cd0aa1a5514adee487d88973a464dd9f619335cc
Devan Goodwin <dgoodwin@redhat.com>
2024-08-27 14:44:24 -0300

    Add views file e2e tests for views and component readiness

--- e2e-scripts/sippy-e2e-sippy-e2e-setup-commands.sh
+++ e2e-scripts/sippy-e2e-sippy-e2e-setup-commands.sh
@@ -185,12 +185,12 @@ spec:
         imagePullPolicy: ${SIPPY_IMAGE_PULL_POLICY:-Always}
         resources:
           limits:
-            memory: 1G
+            memory: 3G
         terminationMessagePath: /dev/termination-log
         terminationMessagePolicy: File
         command:  ["/bin/sh", "-c"]
         args:
-          - /bin/sippy load --init-database --load-openshift-ci-bigquery    --log-level=debug --release 4.14 --database-dsn=postgresql://postgres:password@postgres.sippy-e2e.svc.cluster.local:5432/postgres --mode=ocp --config ./config/e2e-openshift.yaml --google-service-account-credential-file /tmp/secrets/gcs-cred
+          - /bin/sippy load --init-database --load-openshift-ci-bigquery    --log-level=debug --release 4.16 --database-dsn=postgresql://postgres:password@postgres.sippy-e2e.svc.cluster.local:5432/postgres --mode=ocp --config ./config/e2e-openshift.yaml --google-service-account-credential-file /tmp/secrets/gcs-cred --component-readiness-views ./config/views.yaml
         env:
         - name: GCS_SA_JSON_PATH
           value: /tmp/secrets/gcs-cred
---
+++ test/e2e/componentreadiness/componentreadiness_test.go
@@ -0,0 +1,26 @@
+package componentreadiness
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/openshift/sippy/pkg/apis/api/componentreport"
+	"github.com/openshift/sippy/test/e2e/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestComponentReadinessViews(t *testing.T) {
+	var views []componentreport.View
+	err := util.SippyRequest("/api/component_readiness/views", &views)
+	require.NoError(t, err, "error making http request")
+	t.Logf("found %d views", len(views))
+	require.Greater(t, len(views), 0, "no views returned, check server cli params")
+
+	// Make a basic request for the first view and ensure we get some data back
+	var report componentreport.ComponentReport
+	err = util.SippyRequest(fmt.Sprintf("/api/component_readiness?view=%s", views[0].Name), &report)
+	require.NoError(t, err, "error making http request")
+	// We expect over 50 components at time of writing, asserting 25 should be safe
+	assert.Greater(t, len(report.Rows), 25, "component report does not have rows we would expect")
+}
