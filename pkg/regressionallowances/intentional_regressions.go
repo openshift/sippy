4a198587bcc858fb2edc8d4a89860a4db916320b
Forrest Babcock <fbabcock@redhat.com>
2024-09-10 10:28:30 -0400

    Include example IntentionalRegressions json

--- pkg/regressionallowances/intentional_regressions.go
+++ pkg/regressionallowances/intentional_regressions.go
@@ -1,6 +1,7 @@
 package regressionallowances
 
 import (
+	_ "embed"
 	"encoding/json"
 	"fmt"
 )
@@ -8,13 +9,73 @@ import (
 // embed regressions415.json
 // var regressions4_15 []byte
 
+// example regressions json
+// [
+// {
+//  "JiraComponent": "OLM",
+//  "TestID": "Operator results:55a75a8aa11231d0ca36a4d65644e1dd",
+//  "TestName": "operator conditions operator-lifecycle-manager-packageserver",
+//  "variant": {
+//   "variants": {
+//    "Architecture": "amd64",
+//    "FeatureSet": "default",
+//    "Installer": "ipi",
+//    "Network": "ovn",
+//    "Platform": "metal",
+//    "Suite": "unknown",
+//    "Topology": "ha",
+//    "Upgrade": "micro"
+//   }
+//  },
+//  "PreviousSuccesses": 166,
+//  "PreviousFailures": 0,
+//  "PreviousFlakes": 0,
+//  "RegressedSuccesses": 57,
+//  "RegressedFailures": 5,
+//  "RegressedFlakes": 0,
+//  "JiraBug": "https://issues.redhat.com/browse/OCPBUGS-33255",
+//  "ReasonToAllowInsteadOfFix": "Waiting for upstream fix to propagate"
+// },
+// {
+//  "JiraComponent": "openshift-apiserver",
+//  "TestID": "Operator results:a4dfe6caa55e94230b4ee0ff127b6d64",
+//  "TestName": "operator conditions openshift-apiserver",
+//  "variant": {
+//   "variants": {
+//    "Architecture": "amd64",
+//    "FeatureSet": "default",
+//    "Installer": "ipi",
+//    "Network": "ovn",
+//    "Platform": "metal",
+//    "Suite": "unknown",
+//    "Topology": "ha",
+//    "Upgrade": "micro"
+//   }
+//  },
+//  "PreviousSuccesses": 166,
+//  "PreviousFailures": 0,
+//  "PreviousFlakes": 0,
+//  "RegressedSuccesses": 57,
+//  "RegressedFailures": 5,
+//  "RegressedFlakes": 0,
+//  "JiraBug": "https://issues.redhat.com/browse/OCPBUGS-33255",
+//  "ReasonToAllowInsteadOfFix": "Waiting for upstream fix to propagate"
+// }
+// ]
+
+
+//go:embed regressions417.json
+var regressions4_17 []byte
+
 var (
 	release415 release = "4.15"
+	release417 release = "4.17"
 )
 
 //nolint:all
 func init() {
 	// importIntentionalRegressions(release415, regressions4_15)
+	importIntentionalRegressions(release417, regressions4_17)
 }
 
 func importIntentionalRegressions(releaseTarget release, jsonRegressions []byte) {
