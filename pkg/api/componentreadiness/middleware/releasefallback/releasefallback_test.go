fd8340e854977a64ea95db345107db9e4ae7d06b
Devan Goodwin <dgoodwin@redhat.com>
2025-02-10 14:23:01 -0400

    Complete unit tests for ReleaseFallback middleware

--- pkg/api/componentreadiness/middleware/releasefallback/releasefallback_test.go
+++ pkg/api/componentreadiness/middleware/releasefallback/releasefallback_test.go
@@ -36,72 +36,134 @@ func Test_Transform(t *testing.T) {
 		Start:   &start418,
 		End:     &end418,
 	}
-	fallbackReleases := &crtype.FallbackReleases{
-		Releases: map[string]crtype.ReleaseTestMap{
-			"4.18": {
-				Release: release418,
-				Tests: map[string]crtype.TestStatus{
-					test1KeyStr: {
-						TestName:     "test 1",
-						TestSuite:    "conformance",
-						Component:    "foo",
-						Capabilities: nil,
-						Variants:     test1Variants,
-						TotalCount:   100,
-						SuccessCount: 100,
-						FlakeCount:   0,
-					},
-				},
-			},
+	fallbackMap418 := crtype.ReleaseTestMap{
+		Release: release418,
+		Tests: map[string]crtype.TestStatus{
+			test1KeyStr: buildTestStatus("test1", test1Variants, 100, 95, 0, nil),
+		},
+	}
+
+	start417 := time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC)
+	end417 := time.Date(2024, 12, 31, 0, 0, 0, 0, time.UTC)
+	release417 := crtype.Release{
+		Release: "4.17",
+		Start:   &start417,
+		End:     &end417,
+	}
+	fallbackMap417 := crtype.ReleaseTestMap{
+		Release: release417,
+		Tests: map[string]crtype.TestStatus{
+			test1KeyStr: buildTestStatus("test1", test1Variants, 100, 98, 0, nil),
 		},
 	}
 
 	tests := []struct {
 		name             string
 		reqOpts          crtype.RequestOptions
-		fallbackReleases *crtype.FallbackReleases
+		fallbackReleases crtype.FallbackReleases
 		baseStatus       map[string]crtype.TestStatus
 		expectedStatus   map[string]crtype.TestStatus
 	}{
 		{
-			name:             "fallback to prior release",
-			reqOpts:          reqOpts419,
-			fallbackReleases: fallbackReleases,
-			baseStatus: map[string]crtype.TestStatus{
-				// Same test but got a little worse in 4.18
-				test1KeyStr: {
-					TestName:     "test 1",
-					TestSuite:    "conformance",
-					Component:    "foo",
-					Capabilities: nil,
-					Variants:     []string{"Arch:amd64", "Platform:aws"},
-					TotalCount:   100,
-					SuccessCount: 93,
-					FlakeCount:   3,
+			name:    "fallback to prior release",
+			reqOpts: reqOpts419,
+			fallbackReleases: crtype.FallbackReleases{
+				Releases: map[string]crtype.ReleaseTestMap{
+					fallbackMap418.Release.Release: fallbackMap418,
 				},
 			},
+			baseStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 93, 0, nil),
+			},
 			expectedStatus: map[string]crtype.TestStatus{
-				test1KeyStr: {
-					TestName:     "test 1",
-					TestSuite:    "conformance",
-					Component:    "foo",
-					Capabilities: nil,
-					Variants:     []string{"Arch:amd64", "Platform:aws"},
-					TotalCount:   100,
-					SuccessCount: 100,
-					FlakeCount:   0,
-					Release:      &release418,
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 95, 0, &release418),
+			},
+		},
+		{
+			name:    "fallback twice to prior release",
+			reqOpts: reqOpts419,
+			fallbackReleases: crtype.FallbackReleases{
+				Releases: map[string]crtype.ReleaseTestMap{
+					fallbackMap418.Release.Release: fallbackMap418,
+					fallbackMap417.Release.Release: fallbackMap417, // 4.17 improves even further
 				},
+			},
+			baseStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 93, 0, nil),
+			},
+			expectedStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 98, 0, &release417),
+			},
+		},
+		{
+			name:    "fallback once to two releases ago",
+			reqOpts: reqOpts419,
+			fallbackReleases: crtype.FallbackReleases{
+				Releases: map[string]crtype.ReleaseTestMap{
+					fallbackMap418.Release.Release: fallbackMap418,
+					fallbackMap417.Release.Release: fallbackMap417, // 4.17 improves even further
+				},
+			},
+			baseStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 97, 0, nil),
+			},
+			expectedStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 98, 0, &release417),
+			},
+		},
+		{
+			name:    "don't fallback to prior release",
+			reqOpts: reqOpts419,
+			fallbackReleases: crtype.FallbackReleases{
+				Releases: map[string]crtype.ReleaseTestMap{
+					fallbackMap418.Release.Release: fallbackMap418,
+				},
+			},
+			baseStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 100, 0, nil),
+			},
+			expectedStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 100, 100, 0, nil),
+			},
+		},
+		{
+			name:    "don't fallback to prior release with insufficient runs",
+			reqOpts: reqOpts419,
+			fallbackReleases: crtype.FallbackReleases{
+				Releases: map[string]crtype.ReleaseTestMap{
+					fallbackMap418.Release.Release: fallbackMap418,
+					fallbackMap417.Release.Release: fallbackMap417,
+				},
+			},
+			baseStatus: map[string]crtype.TestStatus{
+				test1KeyStr: buildTestStatus("test1", test1Variants, 10000, 9700, 0, nil),
+			},
+			expectedStatus: map[string]crtype.TestStatus{
+				// No fallback release had at least 60% of our run count
+				test1KeyStr: buildTestStatus("test1", test1Variants, 10000, 9700, 0, nil),
 			},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			rfb := NewReleaseFallbackMiddleware(nil, test.reqOpts)
-			rfb.cachedFallbackTestStatuses = test.fallbackReleases
+			rfb.cachedFallbackTestStatuses = &test.fallbackReleases
 			baseStatus, _, err := rfb.Transform(test.baseStatus, map[string]crtype.TestStatus{})
 			assert.NoError(t, err)
 			assert.Equal(t, test.expectedStatus, baseStatus)
 		})
+	}
+}
+func buildTestStatus(testName string, variants []string, total, success, flake int, release *crtype.Release) crtype.TestStatus {
+	return crtype.TestStatus{
+		TestName:     testName,
+		TestSuite:    "conformance",
+		Component:    "foo",
+		Capabilities: nil,
+		Variants:     variants,
+		TotalCount:   total,
+		SuccessCount: success,
+		FlakeCount:   flake,
+		Release:      release,
 	}
 }
