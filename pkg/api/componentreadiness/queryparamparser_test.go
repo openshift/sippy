72e5c9066e6004e7bb59883face247dc500e7138
Luke Meyer <lmeyer@redhat.com>
2025-08-04 14:54:21 -0400

    TRT-2235: include LayeredProduct in DBGroupBy

--- pkg/api/componentreadiness/queryparamparser_test.go
+++ pkg/api/componentreadiness/queryparamparser_test.go
@@ -234,7 +234,7 @@ func TestParseComponentReportRequest(t *testing.T) {
 			},
 			variantOption: reqopts.Variants{
 				ColumnGroupBy: sets.NewString("Platform", "Architecture", "Network"),
-				DBGroupBy:     sets.NewString("Platform", "Architecture", "Network", "Topology", "Suite", "FeatureSet", "Upgrade", "Installer"),
+				DBGroupBy:     sets.NewString("Platform", "Architecture", "Network", "Topology", "Suite", "FeatureSet", "Upgrade", "Installer", "LayeredProduct"),
 				IncludeVariants: map[string][]string{
 					"Architecture": {"amd64"},
 					"FeatureSet":   {"default", "techpreview"},
