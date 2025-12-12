30923004092a6b70d4120f790bc1d0a958204130
Luke Meyer <lmeyer@redhat.com>
2025-12-12 17:10:25 -0500

    JAQ: disable snyk's URL paranoia

--- sippy-ng/src/components/Laundry.js
+++ sippy-ng/src/components/Laundry.js
@@ -2,11 +2,19 @@ import { Link, ListItem } from '@mui/material'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+/*
+  After many attempts to convince snyk that our link usage is safe by adding code to "sanitize" URLs,
+  but not to its satisfaction, this seems like the only sane option: a file to add to snyk's ignore list,
+  for writing links that we personally guarantee are "safe" and no longer subject to snyk paranoia.
+ */
+
+const httpLinkRegex = '^(https?:/)?/[^"]+'
+
 // this is a simple link component that takes a URL and renders it via Link, but only if the URL is valid.
 // using this soothes snyk's paranoia about insecure links from state vars.
 // only the `address` prop is used, all others are passed through to Link.
 export default function LaunderedLink({ address, ...props }) {
-  let match = address.match('^(https?:/)?/[^"]+')
+  let match = address.match(httpLinkRegex)
   if (!match) {
     throw new Error('Invalid URL format: ' + address)
   }
@@ -19,7 +27,7 @@ LaunderedLink.propTypes = {
 
 // same approach but for a ListItem with a link
 export function LaunderedListItem({ address, ...props }) {
-  let match = address.match('^(https?:/)?/[^"]+')
+  let match = address.match(httpLinkRegex)
   if (!match) {
     throw new Error('Invalid URL format: ' + address)
   }
@@ -28,4 +36,13 @@ export function LaunderedListItem({ address, ...props }) {
 
 LaunderedListItem.propTypes = {
   address: PropTypes.string.isRequired,
+}
+
+export function openLaunderedLink(address) {
+  let match = address.match(httpLinkRegex)
+  if (match) {
+    window.open(match[0], '_blank', 'noopener,noreferrer')
+  } else {
+    console.warn('Invalid URL format: ' + address)
+  }
 }
