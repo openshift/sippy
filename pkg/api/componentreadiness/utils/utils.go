ac3b57eaac27f84ca664b4bbea35d161d206359a
Stephen Goeddel <sgoeddel@redhat.com>
2026-02-09 13:27:29 -0500

    adjust test_details report job names for the migration in openshift/release from master to main

--- pkg/api/componentreadiness/utils/utils.go
+++ pkg/api/componentreadiness/utils/utils.go
@@ -48,6 +48,9 @@ func NormalizeProwJobName(prowName string) string {
 	// Some jobs encode frequency in their name, which can change
 	prowName = regexp.MustCompile(`-f\d+`).ReplaceAllString(prowName, "-fXX")
 
+	// openshift/release migrated from master to main, normalize it
+	prowName = regexp.MustCompile(`openshift-release-master`).ReplaceAllString(prowName, "openshift-release-main")
+
 	return prowName
 }
 
