f31be72895d16c61ad1d4b8ef3c89a2b11ba6c3d
oharan2 <oharan@redhat.com>
2026-06-15 12:08:09 +0300

    Add chaos jobs on 4.22

--- pkg/variantregistry/ocp.go
+++ pkg/variantregistry/ocp.go
@@ -1284,6 +1284,7 @@ func setLayeredProduct(_ logrus.FieldLogger, variants map[string]string, jobName
 		{"-lpga-lp-ocp-compat-cr--oadp-", "lp-ocp-compat--oadp--lpGA"},
 		{"-lpga-lp-ocp-compat-cr--servicemesh-", "lp-ocp-compat--servicemesh--lpGA"},
 		{"-lpga-lp-ocp-compat-cr--operator-e2e-", "lp-ocp-compat--serverless--lpGA"},
+		{"-lpga-lp-chaos-cr--ocp-", "lp-chaos--ocp--lpGA"},
 		{"-coo-", "lp-interop-coo"},
 		{"-virt", "virt"},
 		{"-cnv", "virt"},
